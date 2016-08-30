7c85ce1a659933799d7f120e9b336424a4fe1dc4
kmsheng <kmsh3ng@gmail.com>
2016-08-30 13:27:51 +0800

    add function handleCalculatorClick

--- lesson5/assets/js/calculator.js
+++ lesson5/assets/js/calculator.js
@@ -5,6 +5,40 @@
   var isShowingResult = false;
 
 
+  function handleCalculatorClick(event) {
+
+    var type = event.target.getAttribute('data-type') || '';
+    var isTypeWithoutSpace = !! type.match(/[\d\.]/);
+
+    if (isShowingResult) {
+      input.value = '';
+      isShowingResult = false;
+    }
+
+    if ('' === type) {
+      return;
+    }
+
+    if ('=' === type) {
+      try {
+        input.value = safeEval(input.value);
+      }
+      catch(err) {
+        console.log('safeEval failed', err);
+        input.value = 'error';
+      }
+      isShowingResult = true;
+      return;
+    }
+
+    if ('ce' === type) {
+      // cut the last character
+      input.value = input.value.slice(0, -1);
+      return;
+    }
+    input.value += isTypeWithoutSpace ? type : (' ' + type + ' ');
+  }
+
   function safeEval(str) {
     /* jshint ignore: start */
     return eval((str || '').replace(/[xX]/, '*'));
