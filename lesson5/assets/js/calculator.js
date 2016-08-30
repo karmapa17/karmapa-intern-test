32abd41f91b73753fb3a5fa4d020073a31999468
kmsheng <kmsh3ng@gmail.com>
2016-08-30 13:27:27 +0800

    add function safeEval

--- lesson5/assets/js/calculator.js
+++ lesson5/assets/js/calculator.js
@@ -5,4 +5,10 @@
   var isShowingResult = false;
 
 
+  function safeEval(str) {
+    /* jshint ignore: start */
+    return eval((str || '').replace(/[xX]/, '*'));
+    /* jshint ignore: end */
+  }
+
 })();
